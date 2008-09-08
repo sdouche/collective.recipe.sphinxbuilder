d02c774d77e8ef09f812eb7be63fb9cb079f8c29
Tarek Ziadé <tarek@ziade.org>
2008-09-08 16:29:09 +0000

    implemeted initial feature
    git-svn-id: http://svn.plone.org/svn/collective/buildout/collective.recipe.sphinxbuilder/trunk@71377 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/recipe/sphinxbuilder/README.txt
+++ collective/recipe/sphinxbuilder/README.txt
@@ -15,6 +15,41 @@ doc-outputs
     Multiple-line value that defines what kind of output to produce. 
     Can be `html`, `latex` or `pdf`. Defaults to `html`.
 
+script-name
+    The name of the script generated. Defaults to the name of the section.    
+
+sphinx-project
+    The name of the project used in Plone. Defaults to Plone.
+
+sphinx-extensions
+    Sphinx extensions in use. Defaults to none. 
+
+sphinx-master 
+    Name of the index file. Defaults to index.
+
+sphinx-year
+    Year of the project. Defaults to current year.
+
+sphinx-suffix
+    File extensions used for reST file. Defaults to .txt
+
+sphinx-author
+    Author. Defaults to Plone Community. 
+
+sphinx-version
+    Version. Defaults to 1.0.
+
+sphinx-release
+    Release. Defaults to 1.0.
+
+sphinx-dot 
+    The prefix of the static and template directory.
+    Defaults to '.' under Linux and '_' under Windows.
+
+sphinx-sep
+    Separate source and build directories. (Y or N) 
+    Defaults to Yes.
+
 Example usage
 =============
 
@@ -39,8 +74,7 @@ Let's run the buildout::
 
 What are we expecting ? 
 
-- a `docs` folder with a Sphinx structure.
-- a script in the `bin` folder to build the docs.
+- `docs` folder with a Sphinx structure::
 
     >>> docs = join(sample_buildout, 'docs')
     >>> ls(docs)
@@ -49,8 +83,57 @@ What are we expecting ?
 
     >>> source = join(docs, 'source')
     >>> ls(source) 
-    d  _static
-    d  _templates
+    d  .static
+    d  .templates
     -  conf.py
     -  index.txt
 
+A script in the `bin` folder to build the docs::
+
+    >>> bin = join(sample_buildout, 'bin')
+    >>> ls(bin)
+    - buildout
+    - sphinx
+
+The content of the script is a simple shell script::
+
+    >>> script = join(sample_buildout, 'bin', 'sphinx')
+    >>> print open(script).read()
+    cd ...docs
+    make html
+   
+If we want `latex` and `pdf`, we need to explicitly define it::
+
+    >>> write('buildout.cfg',
+    ... """
+    ... [buildout]
+    ... parts = sphinx
+    ...
+    ... [sphinx]
+    ... recipe = collective.recipe.sphinxbuilder
+    ... doc-outputs =
+    ...     html
+    ...     latex
+    ...     pdf
+    ... """) 
+    >>> print 'start', system(buildout)
+    start...
+    Installing sphinx.
+    <BLANKLINE>
+
+Let's see our script now::
+    
+    >>> print open(script).read() 
+    cd ...docs
+    make html
+    make latex
+    make latex && cd ... && make
+
+Finally let's run it::
+
+    >>> print 'start', system(script)
+    start mkdir -p build/html build/doctrees
+    ...
+    Transcript written in modPlone.ilg.
+    <BLANKLINE>
+ 
--- collective/recipe/sphinxbuilder/__init__.py
+++ collective/recipe/sphinxbuilder/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """Recipe sphinxbuilder"""
 from os.path import join, exists
+from datetime import datetime
 import os
+import sys
 
 from sphinx.quickstart import QUICKSTART_CONF
 from sphinx.quickstart import MAKEFILE
 from sphinx.quickstart import MASTER_FILE
+from sphinx.util import make_filename
 
 class Recipe(object):
     """zc.buildout recipe"""
@@ -23,15 +26,49 @@ class Recipe(object):
     def install(self):
         """Installer"""
         root = self.buildout['buildout']['directory']
+        bin = self.buildout['buildout']['bin-directory']
+        script_name = self.options.get('script-name', self.name) 
         doc_directory = self.options.get('doc-directory', 
                                          join(root, 'docs'))
-        
+
+        if sys.platform == 'win32':
+            DOT = '_'
+        else:
+            DOT = '.'
+
+        self.options['now'] = datetime.now().ctime() 
+        year = datetime.now().year      
+        suffix = '.txt'
+        for name, val in (('project', 'Plone'),  
+                          ('extensions', ''), 
+                          ('master', 'index'),
+                          ('year', str(year)),
+                          ('suffix', suffix), 
+                          ('author', 'Plone Community'),
+                          ('version', '1.0'), 
+                          ('release', '1.0'),
+                          ('dot', DOT),
+                          ('sep', 'yes')):
+            bname = 'sphinx-%s' 
+            self.options[name] = self.options.get(bname, val)          
+
+        self.options['project_fn'] = make_filename(self.options['project'])
+        separate = self.options['sep'].upper() in ('Y', 'YES')
+        self.options['rsrcdir'] = separate and 'source' or '.'
+        self.options['rbuilddir'] = separate and 'build' or DOT + 'build'
+ 
+        lines = [l for l in 
+                 self.options.get('doc-outputs', 'html').split('\n')
+                 if l.strip() != '']
+
+        doc_outputs = [e.strip() for e in lines]
+
         if not exists(doc_directory):
             os.mkdir(doc_directory)
             
             # let's create the initial structure
-            conf = QUICKSTART_CONF
-            make = MAKEFILE
+            conf = QUICKSTART_CONF % self.options
+            make = MAKEFILE % self.options
             master = MASTER_FILE
 
             # Makefile
@@ -48,14 +85,26 @@ class Recipe(object):
             index_file = join(source_dir, 'index.txt')
             self._write_file(index_file, master)
             
-            os.mkdir(join(source_dir, '_templates'))
-            os.mkdir(join(source_dir, '_static'))  
+            os.mkdir(join(source_dir, '%stemplates' % DOT))
+            os.mkdir(join(source_dir, '%sstatic' % DOT))  
 
             # we will add goodies here later
 
-        # now lets create the scripts used to generate docs
-        # XXX todo
-        return tuple()
+        # now lets create the script used to generate docs
+        latex_directory = os.path.join(doc_directory, 'build',
+                                       'latex')
+        script = ['cd %s' % doc_directory]
+        if 'html' in doc_outputs:
+            script.append('make html')
+        if 'latex' in doc_outputs:
+            script.append('make latex')
+        if 'pdf' in doc_outputs:
+            script.append('make latex && cd %s && make' % latex_directory)
+
+        make_doc = join('bin', script_name)
+        self._write_file(make_doc, '\n'.join(script)) 
+        os.chmod(make_doc, 0777) 
+        return (make_doc,)
 
     update = install
 
