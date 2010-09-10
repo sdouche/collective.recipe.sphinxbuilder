07d9da143df1da5b13fb766403c1d9d1465a2c12
Andreas Jung <lists@zopyx.org>
2010-09-10 06:16:11 +0000

    requires Sphinx 1.0 or better
    git-svn-id: http://svn.plone.org/svn/collective/buildout/collective.recipe.sphinxbuilder/trunk@125259 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.6.3.3'
-sphinx_version = '>=0.6.4,<0.7dev'
+version = '0.7.0'
+sphinx_version = '>=1.0'
 
 long_description = (
     read('README.txt')
